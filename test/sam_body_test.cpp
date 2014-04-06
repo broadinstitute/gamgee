2e4f843b6503bcf19d2b6e8852ceefe4189459d1
Mauricio Carneiro <carneiro@broadinstitute.org>
2014-04-06 17:59:07 -0400

    Setters for the simple members of the Sam API
    only those whose data can be accessed independently inside the bam1_t
    and bam_core_t structs. Basically the ones with fixed length.
    
    * add simple members for alignment fields
    * add simple flags
    * add tests to all functions
    
    this closes #13

---
+++ test/sam_body_test.cpp
@@ -0,0 +1,67 @@
+#include <boost/test/unit_test.hpp>
+#include "../gamgee/sam_body.h"
+#include "../gamgee/sam_reader.h"
+
+using namespace std;
+using namespace gamgee;
+
+BOOST_AUTO_TEST_CASE( sam_body_simple_members ) {
+  const auto chr = 5u;
+  const auto aln = 1000u;
+  for (auto s : SamReader<SamIterator> {"testdata/test_simple.bam"}) {  // should be replaced by mock sam_body
+    auto record = s.body();
+    record.set_chromosome(chr);
+    BOOST_CHECK_EQUAL(record.chromosome(), chr);
+    record.set_alignment_start(aln);
+    BOOST_CHECK_EQUAL(record.alignment_start(), aln);
+    record.set_mate_chromosome(chr);
+    BOOST_CHECK_EQUAL(record.mate_chromosome(), chr);
+    record.set_mate_alignment_start(aln);
+    BOOST_CHECK_EQUAL(record.mate_alignment_start(), aln);
+    break;
+  }
+}
+
+BOOST_AUTO_TEST_CASE( sam_body_flags ) 
+{
+  for (auto s : SamReader<SamIterator> {"testdata/test_simple.bam"}) {  // should be replaced by mock sam_body
+    auto record = s.body();
+    record.set_paired();
+    BOOST_CHECK(record.paired());
+    record.set_not_paired();
+    BOOST_CHECK(!record.paired());
+    record.set_unmapped();
+    BOOST_CHECK(record.unmapped());
+    record.set_not_unmapped();
+    BOOST_CHECK(!record.unmapped());
+    record.set_next_unmapped();
+    BOOST_CHECK(record.next_unmapped());
+    record.set_not_next_unmapped();
+    BOOST_CHECK(!record.next_unmapped());
+    record.set_first();
+    BOOST_CHECK(record.first());
+    record.set_not_first();
+    BOOST_CHECK(!record.first());
+    record.set_last();
+    BOOST_CHECK(record.last());
+    record.set_not_last();
+    BOOST_CHECK(!record.last());
+    record.set_secondary();
+    BOOST_CHECK(record.secondary());
+    record.set_not_secondary();
+    BOOST_CHECK(!record.secondary());
+    record.set_fail();
+    BOOST_CHECK(record.fail());
+    record.set_not_fail();
+    BOOST_CHECK(!record.fail());
+    record.set_duplicate();
+    BOOST_CHECK(record.duplicate());
+    record.set_not_duplicate();
+    BOOST_CHECK(!record.duplicate());
+    record.set_supplementary();
+    BOOST_CHECK(record.supplementary());
+    record.set_not_supplementary();
+    BOOST_CHECK(!record.supplementary());
+    break;  // only do this to one read
+  }
+}
