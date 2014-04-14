24a2b54f2e490cfde47aee5fb2d543e0f929667c
Mauricio Carneiro <carneiro@broadinstitute.org>
2014-04-14 16:56:49 -0400

    add binary option to sam writer

--- gamgee/sam_writer.cpp
+++ gamgee/sam_writer.cpp
@@ -2,12 +2,12 @@
 
 namespace gamgee {
 
-SamWriter::SamWriter(const std::string& output_fname) :
-  m_out_file {open_file(output_fname)}
+SamWriter::SamWriter(const std::string& output_fname, const bool binary) :
+  m_out_file {open_file(output_fname, binary ? "wb" : "w")}
 {}
 
-SamWriter::SamWriter(const SamHeader& sam, const std::string& output_fname) :
-  m_out_file {open_file(output_fname)},
+SamWriter::SamWriter(const SamHeader& sam, const std::string& output_fname, const bool binary) :
+  m_out_file {open_file(output_fname, binary ? "wb" : "w")},
   m_header{sam}
 {}
 
@@ -24,8 +24,8 @@ void SamWriter::add_record(const SamBody& body) {
   sam_write1(m_out_file, m_header.m_header, body.m_body); 
 }
 
-htsFile* SamWriter::open_file(const std::string& output_fname) {
-  return hts_open(output_fname.empty() ? "-" : output_fname.c_str(), "w");
+htsFile* SamWriter::open_file(const std::string& output_fname, const std::string& mode) {
+  return hts_open(output_fname.empty() ? "-" : output_fname.c_str(), mode.c_str());
 }
 
 }
