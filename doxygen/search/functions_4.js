8a858dcf155a7b2152aed1f407b41e6cf589be37
travis-ci <travis@travis-ci.org>
2014-06-16 16:36:44 +0000

    Latest doxygen documentation on successful travis build 255 auto-pushed

--- doxygen/search/functions_4.js
+++ doxygen/search/functions_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['empty',['empty',['../classgamgee_1_1_sam.html#a788d9ba6ce6f28d1ddb029b601a3ffef',1,'gamgee::Sam::empty()'],['../classgamgee_1_1_variant_field.html#ad9ad6f02f6ed9cf3861590b291a0aacd',1,'gamgee::VariantField::empty()']]],
+  ['empty',['empty',['../classgamgee_1_1_sam.html#a788d9ba6ce6f28d1ddb029b601a3ffef',1,'gamgee::Sam::empty()'],['../classgamgee_1_1_variant_field.html#a8fde1837b172b85f1f7f8e5dfd7ec52d',1,'gamgee::VariantField::empty()']]],
   ['end',['end',['../classgamgee_1_1_fastq_reader.html#af8d95b9f25cd4428d696a7fd857c58e8',1,'gamgee::FastqReader::end()'],['../classgamgee_1_1_sam_reader.html#ad83ea34dc8c04c684f6e06687da24e60',1,'gamgee::SamReader::end()'],['../classgamgee_1_1_variant_field.html#abdda90506f6e8dfc5ff28086a7b151f4',1,'gamgee::VariantField::end()'],['../classgamgee_1_1_variant_field_value.html#a841c76753ec5468556d76b189d50ac1e',1,'gamgee::VariantFieldValue::end()'],['../classgamgee_1_1_variant_reader.html#a4c8c64a7d3666d46c7e0969469a23605',1,'gamgee::VariantReader::end()']]]
 ];
