263225c85b7fc3ceb42891e1da07f44aa050e93a
travis-ci <travis@travis-ci.org>
2014-06-19 16:45:40 +0000

    Latest doxygen documentation on successful travis build 270 auto-pushed

--- doxygen/search/functions_6.js
+++ doxygen/search/functions_6.js
@@ -1,12 +1,8 @@
 var searchData=
 [
-  ['generic_5fboolean_5finfo_5ffield',['generic_boolean_info_field',['../classgamgee_1_1_variant.html#aaaef3ac0c8bc4457b6cdb0e0b52c79e5',1,'gamgee::Variant']]],
   ['generic_5ffloat_5fformat_5ffield',['generic_float_format_field',['../classgamgee_1_1_variant.html#af6e164067495e6177b41474b3e671d71',1,'gamgee::Variant']]],
-  ['generic_5ffloat_5finfo_5ffield',['generic_float_info_field',['../classgamgee_1_1_variant.html#a2e6b21c0c206069a70dba97add91ae79',1,'gamgee::Variant']]],
   ['generic_5finteger_5fformat_5ffield',['generic_integer_format_field',['../classgamgee_1_1_variant.html#a3d2b061ebd2c8cc79556af020d604775',1,'gamgee::Variant']]],
-  ['generic_5finteger_5finfo_5ffield',['generic_integer_info_field',['../classgamgee_1_1_variant.html#ab8ef4ca0715319c7abb06074e510ab7a',1,'gamgee::Variant']]],
   ['generic_5fstring_5fformat_5ffield',['generic_string_format_field',['../classgamgee_1_1_variant.html#aca6f969c4ad2111899804796a9dffa8e',1,'gamgee::Variant']]],
-  ['generic_5fstring_5finfo_5ffield',['generic_string_info_field',['../classgamgee_1_1_variant.html#af86522f92d843d6922b273fdb2b5d39b',1,'gamgee::Variant']]],
   ['genotype_5fquals',['genotype_quals',['../classgamgee_1_1_variant.html#ab6778edaff78a604b35c5fd8a72648fa',1,'gamgee::Variant']]],
   ['getcompilationinfoforfile',['GetCompilationInfoForFile',['../_8ycm__extra__conf_8py.html#a1c7294d0bfbd409f9d5d418ed3e91a52',1,'.ycm_extra_conf.py']]]
 ];
--- doxygen/search/functions_7.js
+++ doxygen/search/functions_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['has_5ffield',['has_field',['../classgamgee_1_1_variant_header.html#add22080ae861285d6a7b9b9eec3bbe99',1,'gamgee::VariantHeader']]],
   ['header',['header',['../classgamgee_1_1_sam.html#af54747dadf6e431283156d9d455d8817',1,'gamgee::Sam::header()'],['../classgamgee_1_1_sam_reader.html#a26871d833999deee363e5619476801ea',1,'gamgee::SamReader::header()'],['../classgamgee_1_1_variant.html#adcf70d251726d37a8a5b7fc3e5ffa0f3',1,'gamgee::Variant::header()'],['../classgamgee_1_1_variant_reader.html#ace9b67be3e4aad24e670a6fd32b70b7c',1,'gamgee::VariantReader::header()']]],
   ['hts_5fstring_5farray_5fto_5fvector',['hts_string_array_to_vector',['../namespacegamgee_1_1utils.html#aa3f1ec8c7415419a0e1c57c8255873a9',1,'gamgee::utils']]]
 ];
