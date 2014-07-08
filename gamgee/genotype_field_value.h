24856fa01dc8381b52d9f07400066a6cfb02a025
Khalid Shakir <kshakir@broadinstitute.org>
2014-07-09 03:03:57 +0800

    Intermediate push with GFV back as subclass of VFV<int32_t>. This doesn't compile as still figuring out how writing to consts actually works in current master VFV. https://github.com/broadinstitute/gamgee/blob/d077c4ee9704d5488f3381142493a28078dfa1d0/gamgee/variant_field_value.h#L99
    Also figuring out overloading of move and other operators.
    Biggest TODO: use GT instead of PL from htslib.

--- gamgee/genotype_field_value.h
+++ gamgee/genotype_field_value.h
@@ -1,7 +1,6 @@
 #ifndef __gamgee__genotype_field_value__
 #define __gamgee__genotype_field_value__
 
-#include "genotype_field_value_iterator.h"
 #include "diploid_pl_genotype.h"
 #include "utils/hts_memory.h"
 #include "utils/utils.h"
@@ -13,24 +12,18 @@
 
 namespace gamgee {
 
-class GenotypeFieldValue {
+class GenotypeFieldValue : public VariantFieldValue<int32_t> {
 
   public:
     GenotypeFieldValue(const std::shared_ptr<bcf1_t>& body, const bcf_fmt_t* const format_ptr, uint8_t* const data_ptr) :
-      m_body {body},
-      m_format_ptr {const_cast<bcf_fmt_t*>(format_ptr)},
-      m_data_ptr {data_ptr},
-      m_num_bytes {utils::size_for_type(static_cast<utils::FormatFieldType>(format_ptr->type))}
+      VariantFieldValue<int32_t>(body, format_ptr, data_ptr)
     {
     }
 
     GenotypeFieldValue(const GenotypeFieldValue& other) = delete;
 
     GenotypeFieldValue(GenotypeFieldValue&& other) :
-      m_body {std::move(other.m_body)},
-      m_format_ptr {other.m_format_ptr},
-      m_data_ptr {other.m_data_ptr},
-      m_num_bytes {other.m_num_bytes}
+      VariantFieldValue<int32_t>(std::move(other))
     {
     }
 
@@ -39,24 +32,8 @@ class GenotypeFieldValue {
     GenotypeFieldValue& operator=(GenotypeFieldValue&& other) {
       if (this != &other)
         return *this;
-      m_body = std::move(other.m_body);
-      m_format_ptr = other.m_format_ptr;
-      m_data_ptr = other.m_data_ptr;
-      m_num_bytes = other.m_num_bytes;
+      VariantFieldValue<int32_t>::operator=(std::move(other));
       return *this;
-    }
-
-    int32_t operator[](const uint32_t index) const {
-      utils::check_boundaries(index, m_format_ptr->n - 1);
-      return convert_from_byte_array(index);
-    }
-
-    GenotypeFieldValueIterator begin() const {
-      return GenotypeFieldValueIterator{m_body, m_data_ptr, m_num_bytes, static_cast<utils::FormatFieldType>(m_format_ptr->type)};
-    }
-
-    GenotypeFieldValueIterator end() const {
-      return GenotypeFieldValueIterator{m_body, m_data_ptr + m_format_ptr->size, m_num_bytes, static_cast<utils::FormatFieldType>(m_format_ptr->type)};
     }
 
     /**
@@ -81,17 +58,9 @@ class GenotypeFieldValue {
     }
 
   private:
-    std::shared_ptr<bcf1_t> m_body;
-    bcf_fmt_t* m_format_ptr;
-    uint8_t* m_data_ptr;
-    uint8_t m_num_bytes;
 
     bool is_this_genotype(const DiploidPLGenotype& genotype) const {
       return (*this)[static_cast<int32_t>(genotype)] == 0;
-    }
-
-    inline int32_t convert_from_byte_array(int index) const {
-      return utils::convert_data_to_integer(m_data_ptr, index, m_num_bytes, static_cast<utils::FormatFieldType>(m_format_ptr->type));
     }
 };
 
