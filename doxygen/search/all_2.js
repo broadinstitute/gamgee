cb1f05b1423455bed28ed0426e13c2546ebbfcb6
travis-ci <travis@travis-ci.org>
2014-07-10 16:27:22 +0000

    Latest doxygen documentation on successful travis build 423 auto-pushed

--- doxygen/search/all_2.js
+++ doxygen/search/all_2.js
@@ -3,6 +3,7 @@ var searchData=
   ['c',['C',['../namespacegamgee.html#ab33f4f3d734f661e6917eb693df3aa49a0d61f8370cad1d412f80b84d143e1257',1,'gamgee']]],
   ['char_5ftag',['char_tag',['../classgamgee_1_1_sam.html#a1271912c9905795941b4dd14d02196a6',1,'gamgee::Sam']]],
   ['check_5fboundaries',['check_boundaries',['../namespacegamgee_1_1utils.html#a68e799fdc7eb14f8784ac77618a87a8f',1,'gamgee::utils']]],
+  ['check_5fmax_5fboundary',['check_max_boundary',['../namespacegamgee_1_1utils.html#a8dc2f050e5f381a43d332c2b12171f66',1,'gamgee::utils']]],
   ['chop',['chop',['../classgamgee_1_1_fastq.html#a7194ef6a3e1799b6e0dbc4817c61455d',1,'gamgee::Fastq']]],
   ['chromosome',['chromosome',['../classgamgee_1_1_sam.html#ac6b0aa48ad5673b617d11b97950e4e91',1,'gamgee::Sam::chromosome()'],['../classgamgee_1_1_variant.html#ae7ff7ad4733aaa8c66a65993b1ae6fa9',1,'gamgee::Variant::chromosome()']]],
   ['chromosomes',['chromosomes',['../classgamgee_1_1_variant_header.html#a636d949964cad4438e133da1639cd87e',1,'gamgee::VariantHeader']]],
