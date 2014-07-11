f2090c04d589a734a36007e9d1b3a11440e92cf3
travis-ci <travis@travis-ci.org>
2014-07-11 05:56:42 +0000

    Latest doxygen documentation on successful travis build 432 auto-pushed

--- doxygen/search/functions_14.js
+++ doxygen/search/functions_14.js
@@ -1,15 +1,4 @@
 var searchData=
 [
-  ['_7ebasequals',['~BaseQuals',['../classgamgee_1_1_base_quals.html#a3ec3a799d46996b4db875afca9a215da',1,'gamgee::BaseQuals']]],
-  ['_7ecigar',['~Cigar',['../classgamgee_1_1_cigar.html#aacb5a081f09996ed7ca1b325b212294e',1,'gamgee::Cigar']]],
-  ['_7efastqreader',['~FastqReader',['../classgamgee_1_1_fastq_reader.html#a5e4856c3af49fcea4f91a568bfb89653',1,'gamgee::FastqReader']]],
-  ['_7ereadbases',['~ReadBases',['../classgamgee_1_1_read_bases.html#a7e63842e54197f854264860bf87a1cf6',1,'gamgee::ReadBases']]],
-  ['_7esambuilder',['~SamBuilder',['../classgamgee_1_1_sam_builder.html#a64f813dc61bb374229e44e2a071eef10',1,'gamgee::SamBuilder']]],
-  ['_7esambuilderdatafield',['~SamBuilderDataField',['../classgamgee_1_1_sam_builder_data_field.html#a1508b3fd9bc91b0e9aac639494aa4fc5',1,'gamgee::SamBuilderDataField']]],
-  ['_7esamreader',['~SamReader',['../classgamgee_1_1_sam_reader.html#a0ad926168662cf163cd4a27e3d723ca4',1,'gamgee::SamReader']]],
-  ['_7esamtag',['~SamTag',['../classgamgee_1_1_sam_tag.html#a5b78cf3f117ba7b9746ee1f3c86b8625',1,'gamgee::SamTag']]],
-  ['_7esamwriter',['~SamWriter',['../classgamgee_1_1_sam_writer.html#abc1d4cbec70ec4d7e01d187cc304b3a4',1,'gamgee::SamWriter']]],
-  ['_7evariantheader',['~VariantHeader',['../classgamgee_1_1_variant_header.html#ac5d4c86162bb98f2e1a3d2b96ccc6055',1,'gamgee::VariantHeader']]],
-  ['_7evariantreader',['~VariantReader',['../classgamgee_1_1_variant_reader.html#ab7a1d2a582f6c257f7ecb02a6ccc71c4',1,'gamgee::VariantReader']]],
-  ['_7evariantwriter',['~VariantWriter',['../classgamgee_1_1_variant_writer.html#aaca49e73bdacb72d0910b3b4c2d53331',1,'gamgee::VariantWriter']]]
+  ['zip',['zip',['../namespacegamgee_1_1utils.html#a3993f3188fb1d3bd6337a36a53efb442',1,'gamgee::utils']]]
 ];
--- doxygen/search/search.js
+++ doxygen/search/search.js
@@ -7,11 +7,11 @@
 
 var indexSectionsWithContent =
 {
-  0: "abcdefghilmnopqrstuvx~",
+  0: "abcdefghilmnopqrstuvxz~",
   1: "bcfhrsv",
   2: "g",
   3: "bcfghirsuv",
-  4: "abcdefghilmnopqrstuv~",
+  4: "abcdefghilmnopqrstuvz~",
   5: "cdfs",
   6: "cps",
   7: "bcdf",
