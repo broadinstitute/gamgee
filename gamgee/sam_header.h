a00e06440dab091a3d01d08dd5f6e14e78a30f45
Mauricio Carneiro <carneiro@broadinstitute.org>
2014-07-30 00:12:39 -0400

    SamHeader: add n_sequences member function
    * add n_sequences member function to allow iteration over the reference sequences
    * make const member functions const (they weren't for some reason)
    * inline simple member functions

--- gamgee/sam_header.h
+++ gamgee/sam_header.h
@@ -13,15 +13,16 @@ namespace gamgee {
  */
 class SamHeader {
  public:
-  explicit SamHeader() = default;                               ///< @brief initializes a null SamHeader @warning if you need to create a SamHeader from scratch, use the builder instead
-  explicit SamHeader(const std::shared_ptr<bam_hdr_t>& header); ///< @brief creates a SamHeader given htslib object. @note used by all iterators
-  SamHeader(const SamHeader& other);                            ///< @brief makes a deep copy of a SamHeader. Shared pointers maintain state to all other associated objects correctly.
-  SamHeader(SamHeader&& other) noexcept;                        ///< @brief moves SamHeader accordingly. Shared pointers maintain state to all other associated objects correctly.
-  SamHeader& operator=(const SamHeader& other);                 ///< @brief deep copy assignment of a SamHeader. Shared pointers maintain state to all other associated objects correctly.
-  SamHeader& operator=(SamHeader&& other) noexcept;             ///< @brief move assignment of a SamHeader. Shared pointers maintain state to all other associated objects correctly.
-  uint32_t sequence_length(const std::string& sequence_name);   ///< @brief Returns the length of the given sequence as stored in the @SQ tag in the BAM header.
-  uint32_t sequence_length(const uint32_t sequence_index);      ///< @brief Returns the length of the given sequence as stored in the @SQ tag in the BAM header.
-  std::string sequence_name(const uint32_t sequence_index);	    ///< @brief Returns the sequence name for the sequence with the given zero-based index
+  explicit SamHeader() = default;                                   ///< @brief initializes a null SamHeader @warning if you need to create a SamHeader from scratch, use the builder instead
+  explicit SamHeader(const std::shared_ptr<bam_hdr_t>& header);     ///< @brief creates a SamHeader given htslib object. @note used by all iterators
+  SamHeader(const SamHeader& other);                                ///< @brief makes a deep copy of a SamHeader. Shared pointers maintain state to all other associated objects correctly.
+  SamHeader(SamHeader&& other) noexcept;                            ///< @brief moves SamHeader accordingly. Shared pointers maintain state to all other associated objects correctly.
+  SamHeader& operator=(const SamHeader& other);                     ///< @brief deep copy assignment of a SamHeader. Shared pointers maintain state to all other associated objects correctly.
+  SamHeader& operator=(SamHeader&& other) noexcept;                 ///< @brief move assignment of a SamHeader. Shared pointers maintain state to all other associated objects correctly.
+  uint32_t n_sequences() const {return m_header->n_targets;}        ///< @brief Returns the number of reference sequences in the header
+  uint32_t sequence_length(const std::string& sequence_name) const; ///< @brief Returns the length of the given reference sequence as stored in the @SQ tag in the BAM header.
+  uint32_t sequence_length(const uint32_t sequence_index) const { return m_header->target_len[sequence_index]; }                ///< @brief Returns the length of the given reference sequence as stored in the @SQ tag in the BAM header.
+  std::string sequence_name(const uint32_t sequence_index) const { return std::string(m_header->target_name[sequence_index]); } ///< @brief Returns the sequence name for the sequence with the given zero-based index
 
  private:
   std::shared_ptr<bam_hdr_t> m_header;
