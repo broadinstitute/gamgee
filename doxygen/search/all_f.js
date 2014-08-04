3875e0740fbd15ea3910ca218968a72668f88570
travis-ci <travis@travis-ci.org>
2014-08-04 19:51:41 +0000

    Latest doxygen documentation on successful travis build 619 auto-pushed

--- doxygen/search/all_f.js
+++ doxygen/search/all_f.js
@@ -4,7 +4,7 @@ var searchData=
   ['read_5fbases_2ecpp',['read_bases.cpp',['../read__bases_8cpp.html',1,'']]],
   ['read_5fbases_2eh',['read_bases.h',['../read__bases_8h.html',1,'']]],
   ['readbases',['ReadBases',['../classgamgee_1_1_read_bases.html',1,'gamgee']]],
-  ['readbases',['ReadBases',['../classgamgee_1_1_read_bases.html#acb41dd049c1fb231ee6f72b55195de53',1,'gamgee::ReadBases::ReadBases(const std::shared_ptr&lt; bam1_t &gt; &amp;sam_record)'],['../classgamgee_1_1_read_bases.html#abbfd890517b0c6a0bc61a498c5594577',1,'gamgee::ReadBases::ReadBases(const ReadBases &amp;other)'],['../classgamgee_1_1_read_bases.html#a7dc61b37d4e840e8ef4245f92c2294db',1,'gamgee::ReadBases::ReadBases(ReadBases &amp;&amp;other) noexcept']]],
+  ['readbases',['ReadBases',['../classgamgee_1_1_read_bases.html#acb41dd049c1fb231ee6f72b55195de53',1,'gamgee::ReadBases::ReadBases(const std::shared_ptr&lt; bam1_t &gt; &amp;sam_record)'],['../classgamgee_1_1_read_bases.html#abbfd890517b0c6a0bc61a498c5594577',1,'gamgee::ReadBases::ReadBases(const ReadBases &amp;other)'],['../classgamgee_1_1_read_bases.html#a5980c16ea147dfcd7da14fec216e6962',1,'gamgee::ReadBases::ReadBases(ReadBases &amp;&amp;other)=default']]],
   ['ref',['ref',['../classgamgee_1_1_variant.html#a5a5d1e31d3c567bf372bae48449d9b52',1,'gamgee::Variant']]],
   ['reverse',['reverse',['../classgamgee_1_1_sam.html#aefde0c304a55102fc1674d985808c993',1,'gamgee::Sam']]],
   ['reverse_5fcomplement',['reverse_complement',['../classgamgee_1_1_fastq.html#a6657a44eb3efa6419cb777f79b13912f',1,'gamgee::Fastq::reverse_complement()'],['../namespacegamgee_1_1utils.html#a740716d2b795a26787bf1000ae62ddb2',1,'gamgee::utils::reverse_complement()']]]
