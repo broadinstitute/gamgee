84be0f9f109b5a17f3e46d95317d540bab2382b3
travis-ci <travis@travis-ci.org>
2014-08-12 15:11:55 +0000

    Latest doxygen documentation on successful travis build 680 auto-pushed

--- doxygen/search/classes_7.js
+++ doxygen/search/classes_7.js
@@ -10,5 +10,6 @@ var searchData=
   ['sampairiterator',['SamPairIterator',['../classgamgee_1_1_sam_pair_iterator.html',1,'gamgee']]],
   ['samreader',['SamReader',['../classgamgee_1_1_sam_reader.html',1,'gamgee']]],
   ['samtag',['SamTag',['../classgamgee_1_1_sam_tag.html',1,'gamgee']]],
-  ['samwriter',['SamWriter',['../classgamgee_1_1_sam_writer.html',1,'gamgee']]]
+  ['samwriter',['SamWriter',['../classgamgee_1_1_sam_writer.html',1,'gamgee']]],
+  ['singleinputexception',['SingleInputException',['../classgamgee_1_1utils_1_1_single_input_exception.html',1,'gamgee::utils']]]
 ];
