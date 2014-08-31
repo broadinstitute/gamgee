6be14e4d6f88cd8faa46df388bfa50cd1874a9bf
Joel Thibault <thibault@broadinstitute.org>
2014-08-31 09:53:16 -0400

    Cache headers instead of deep copying every time

--- gamgee/synced_variant_iterator.cpp
+++ gamgee/synced_variant_iterator.cpp
@@ -37,6 +37,17 @@ bool SyncedVariantIterator::operator!=(const SyncedVariantIterator& rhs) {
 }
 
 /**
+ * @brief initializes m_headers_vector
+ */
+void SyncedVariantIterator::init_headers_vector() {
+  m_headers_vector.reserve(m_synced_readers->nreaders);
+  for (int idx = 0; idx < m_synced_readers->nreaders; idx++) {
+    auto* hdr_ptr = utils::variant_header_deep_copy(bcf_sr_get_header(m_synced_readers.get(), idx));
+    m_headers_vector.emplace_back(utils::make_shared_variant_header(hdr_ptr));
+  }
+}
+
+/**
  * @brief pre-fetches the next variant record
  */
 void SyncedVariantIterator::fetch_next_record() {
@@ -44,11 +55,14 @@ void SyncedVariantIterator::fetch_next_record() {
   if (!bcf_sr_next_line(m_synced_readers.get()))
     return;
 
+  // can't initialize until a line has been read
+  if (m_headers_vector.empty())
+    init_headers_vector();
+
   for (int idx = 0; idx < m_synced_readers->nreaders; idx++) {
     if (bcf_sr_has_line(m_synced_readers.get(), idx)) {
-      auto* hdr_ptr = utils::variant_header_deep_copy(bcf_sr_get_header(m_synced_readers.get(), idx));
       auto* body_ptr = utils::variant_deep_copy(bcf_sr_get_line(m_synced_readers.get(), idx));
-      m_variant_vector.emplace_back(utils::make_shared_variant_header(hdr_ptr), utils::make_shared_variant(body_ptr));
+      m_variant_vector.emplace_back(m_headers_vector[idx], utils::make_shared_variant(body_ptr));
     }
   }
 }
