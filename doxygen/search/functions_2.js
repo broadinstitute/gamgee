952937ddb89e0e3142f51cde6f89c2cebc8f65a9
travis-ci <travis@travis-ci.org>
2014-10-26 21:41:45 +0000

    Latest doxygen documentation on successful travis build 1247 auto-pushed

--- doxygen/search/functions_2.js
+++ doxygen/search/functions_2.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['cache_5fand_5fadvance_5fto_5fend_5fif_5fnecessary',['cache_and_advance_to_end_if_necessary',['../namespacegamgee_1_1utils.html#abedab4665606056c0425680e3db92cee',1,'gamgee::utils']]],
   ['char_5ftag',['char_tag',['../classgamgee_1_1_sam.html#a1271912c9905795941b4dd14d02196a6',1,'gamgee::Sam']]],
-  ['check_5fmax_5fboundary',['check_max_boundary',['../namespacegamgee_1_1utils.html#a0f228e0091b6df37b21d4a58c7ad372c',1,'gamgee::utils']]],
+  ['check_5fmax_5fboundary',['check_max_boundary',['../namespacegamgee_1_1utils.html#a7dc9761d65ea9b619029ea0f7ee1372f',1,'gamgee::utils::check_max_boundary(const uint32_t index, const uint32_t size, const std::string &amp;prefix_msg)'],['../namespacegamgee_1_1utils.html#a0f228e0091b6df37b21d4a58c7ad372c',1,'gamgee::utils::check_max_boundary(const uint32_t index, const uint32_t size)']]],
   ['chop',['chop',['../classgamgee_1_1_fastq.html#a7194ef6a3e1799b6e0dbc4817c61455d',1,'gamgee::Fastq']]],
   ['chr',['chr',['../classgamgee_1_1_interval.html#ad603748829718238d8d408f2bc0ed5be',1,'gamgee::Interval']]],
   ['chromosome',['chromosome',['../classgamgee_1_1_sam.html#ac6b0aa48ad5673b617d11b97950e4e91',1,'gamgee::Sam::chromosome()'],['../classgamgee_1_1_variant.html#ae7ff7ad4733aaa8c66a65993b1ae6fa9',1,'gamgee::Variant::chromosome()']]],
@@ -13,6 +13,7 @@ var searchData=
   ['cigar',['Cigar',['../classgamgee_1_1_cigar.html#a877e40f18e0301cd5a7fd79009e8d793',1,'gamgee::Cigar::Cigar(const std::shared_ptr&lt; bam1_t &gt; &amp;sam_record)'],['../classgamgee_1_1_cigar.html#ad5b1007650ff371984e1c453b5d36a0f',1,'gamgee::Cigar::Cigar(const Cigar &amp;other)'],['../classgamgee_1_1_cigar.html#ad2de41ad99827a297e3ec07b39bc5b6e',1,'gamgee::Cigar::Cigar(Cigar &amp;&amp;other)=default'],['../classgamgee_1_1_sam.html#a3f83688f6ff250f0d2110dfa6e0f6d86',1,'gamgee::Sam::cigar()']]],
   ['cigar_5fop',['cigar_op',['../classgamgee_1_1_cigar.html#a006cfbb4b6187c8a09f84240071c38f6',1,'gamgee::Cigar']]],
   ['cigar_5foplen',['cigar_oplen',['../classgamgee_1_1_cigar.html#afabc7802c54abc14ea8c6a8e577b7894',1,'gamgee::Cigar']]],
+  ['combined_5fheader',['combined_header',['../classgamgee_1_1_multiple_variant_reader.html#a38335aed90579cc4fb9c9b066788c881',1,'gamgee::MultipleVariantReader']]],
   ['comment',['comment',['../classgamgee_1_1_fastq.html#a45b0dd799a8bb650ad19746b30dca544',1,'gamgee::Fastq']]],
   ['complement',['complement',['../namespacegamgee_1_1utils.html#af406bde810c85ea1749461ed137ab59b',1,'gamgee::utils::complement(std::string &amp;sequence)'],['../namespacegamgee_1_1utils.html#a7a60af0f48035150f82481f2547c57e9',1,'gamgee::utils::complement(const std::string &amp;sequence)'],['../namespacegamgee_1_1utils.html#ab9002b9228221fb6a03d04e48424e318',1,'gamgee::utils::complement(const char base)']]],
   ['complement_5fbase',['complement_base',['../namespacegamgee_1_1utils.html#afa77e0e092d799249e1843b35b6fc33a',1,'gamgee::utils']]],
