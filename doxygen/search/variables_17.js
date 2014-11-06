9eab6c32a81f9e4c300eda0b03c8597cfa8e1908
travis-ci <travis@travis-ci.org>
2014-11-06 01:29:18 +0000

    Latest doxygen documentation on successful travis build 1357 auto-pushed

--- doxygen/search/variables_17.js
+++ doxygen/search/variables_17.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['x',['X',['../structcram__feature.html#ada38256758a25a4a846147ecc0dd8ac1',1,'cram_feature::X()'],['../structcstate__t.html#a45e897f695357d0f41b848a6067995b7',1,'cstate_t::x()']]]
+  ['x',['x',['../structcstate__t.html#a45e897f695357d0f41b848a6067995b7',1,'cstate_t::x()'],['../structcram__feature.html#ada38256758a25a4a846147ecc0dd8ac1',1,'cram_feature::X()']]]
 ];
