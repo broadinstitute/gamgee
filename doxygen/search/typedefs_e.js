c6c26d194d2753a6d97d8386be13f1798ff7893c
travis-ci <travis@travis-ci.org>
2014-11-19 18:33:17 +0000

    Latest doxygen documentation on successful travis build 1454 auto-pushed

--- doxygen/search/typedefs_e.js
+++ doxygen/search/typedefs_e.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['variantindexpair',['VariantIndexPair',['../namespacegamgee.html#a4eed5186f534f70971cb415ed1297936',1,'gamgee']]],
+  ['variantiteratorindexpair',['VariantIteratorIndexPair',['../namespacegamgee.html#a2b23ebf9f3717c0d0593ac85af1a721a',1,'gamgee']]],
   ['vcffile',['vcfFile',['../vcf_8h.html#aa14d3052e222f49dde35603b4ba07b15',1,'vcf.h']]]
 ];
